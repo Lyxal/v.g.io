4dfeb63958b6b8ca7188815a49f774df91221e13
lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-11 15:06:02 +1100

    impot

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [vue(), ViteYaml()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./v.g.io/src", import.meta.url)),
     },
   },
 });
