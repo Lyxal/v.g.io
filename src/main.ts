86fb2e127b1e6d0fac4f21aeeb0794e5ce7baea5
lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-11 15:13:49 +1100

    ..?

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
-import "v.g.io/src/assets/main.css";
+import "@/assets/main.css";
 
 const pinia = createPinia();
 const app = createApp(App);
